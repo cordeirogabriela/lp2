950dc9f99e1ae90e1223d5eb47652f767df6ea9c
gbncordeiro <89927270+gbncordeiro@users.noreply.github.com>
2022-04-12 19:00:45 -0300

    Aula 12/04/2022

---
+++ application/controllers/api/ContasRest.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/rest/MyRestController.php';
+
+class ContasRest extends MyRestController {
+	
+	function __construct() {
+		parent::__construct ('contas');
+	}
+
+	function delete_conta_post(){
+		$res = $this->model->delete_conta();
+		$this->response($res, RESTController::HTTP_OK);
+    }
+
+    function status_conta_post() {
+        $res = $this->model->status_conta();
+        $this->response($res, RESTController::HTTP_OK);
+    }
+	
+}
---
+++ application/views/common/rodape.php
@@ -0,0 +1,8 @@
+
+        <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/popper.min.js"></script>
+        <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?= base_url() ?>assets/mdb/js/mdb.min.js"></script>
+        <script type="text/javascript" src="<?= base_url() ?>assets/js/util.js"></script>
+
+    </body>
+</html>
